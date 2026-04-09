f0aedd720b25d4bd899482d9567c679a7d39d811
glauciojsilveira <glauciojsilveira@hotmail.com>
2026-04-09 15:18:32 -0300

    QA: Configuração de pipeline finalizada e Hooks ajustado

--- src/test/java/com/automacao/steps/Runner.java
+++ src/test/java/com/automacao/steps/Runner.java
@@ -8,11 +8,10 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         features = "src/test/resources/features", // Onde estão seus arquivos .feature
         glue = "com.automacao.steps",                        // Onde estão suas classes Java de Step Definitions
-        plugin = {"pretty", "html:target/cucumber-reports.html"}, // Gera um relatório HTML simples no 'target'
+        plugin = {"pretty", "html:target/cucumber-reports.html", "json:target/cucumber.json"}, // Gera HTML e JSON para relatórios
         monochrome = true,                      // Deixa a saída do terminal mais legível
         tags = "not @wip"                      // Executa todos os testes que NÃO possuem a tag @wip (Work In Progress)
 )
 public class Runner {
         // Esta classe não precisa de corpo, as anotações acima fazem todo o trabalho.
 }
-
